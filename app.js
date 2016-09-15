55c2ef9ffb14c3c2a29f010746021f9f337ea2b3
Matthew Steinbergs <mj.steinbergs@gmail.com>
2016-09-15 16:39:00 -0500

    employee route

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use( express.static( 'node_modules/jquery/dist' ) );
 app.use( express.static( 'node_modules/normalize.css' ) );
 
 // Set Port
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 2789;
 
 // Listen on port
 app.listen( port, function () {
@@ -36,31 +36,70 @@ app.get( '/', function( req, res ) {
 });
 app.use( express.static( 'node_modules/jquery/dist/') );
 
-app.post('/employee', function(req,res){
-	console.log('/employee hit', req.body);
-	var first_name = req.body.first_name;
-	var last_name = req.body.last_name;
+app.route('/employee')
+	.post((req,res) => {
+		console.log('/employee hit', req.body);
+		var first_name = req.body.first_name;
+		var last_name = req.body.last_name;
 
-	pg.connect(connectionString, function(err, client, done){
-		if(err){
-			console.log(err);
-		} else {
-			console.log('connected to db');
-			console.log(first_name);
-			client.query('INSERT INTO server(first_name, last_name) VALUES($1, $2)', [first_name, last_name]);
-			res.send({success: true});
-		}
+		pg.connect(connectionString, function(err, client, done){
+			if(err){
+				console.log(err);
+			} else {
+				console.log('connected to db');
+				console.log(first_name);
+				client.query('INSERT INTO server(first_name, last_name) VALUES($1, $2)', [first_name, last_name]);
+				res.send({success: true});
+			}
+		});
+	})
+	.get(function(req, res){
+		console.log('/employee base hit');
+
+		pg.connect(connectionString, (err, client, done) => {
+			if(err) res.status(500).send( "Whoops!");
+
+				var resultsArray = [];
+				var query = client.query('SELECT * FROM server ORDER BY last_name ASC');
+
+				query.on('row', (row) => {
+					resultsArray.push(row);
+				});
+
+				query.on('end', () => {
+					done();
+					return res.send(resultsArray);
+				});
+		});
+
+	})
+	.put(function(req, res){
+		var data = req.body;
+
+		console.log( "Data Recieved:", data );
+
+		  pg.connect( connectionString, ( err, client, done ) => {
+
+		    if( err ) res.status(500).send( "Oops!");
+
+		    var resultsArray = [];
+		    var query = client.query( 'UPDATE server SET "status"=$1 WHERE "id"=$2', [data.status,  data.id]);
+
+		    query.on( 'row', ( row ) => {
+		      resultsArray.push( row );
+		    });
+
+		    query.on( 'end', ( ) => {
+		      done();
+		      return res.status(200).send( { status: "Okay" });
+		    });
+		  });
 	});
-
-
-});
-});
 
 /* -- /booth Route -- */
 app.route( '/booth' )
 .get( ( req, res ) => {
 
-	var objectToSend= {}; //end objectToSend
 	  pg.connect( connectionString, ( err, client, done ) => {
 
 	    if( err ) res.status(500).send( "Oops!");
@@ -78,7 +117,7 @@ app.route( '/booth' )
 	    });
 	  });
 
-} )
+})
 .post( ( req, res ) => {
 
 	var data = req.body;
