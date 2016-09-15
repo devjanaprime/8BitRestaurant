35f8bdeb13e6c1dd625eda820dcdc63329c70a02
Cody Ogden <cody@codyogden.com>
2016-09-15 13:54:40 -0500

    Spin Up Server. Skeleton Ready. Server handshake running.

---
+++ app.js
@@ -0,0 +1,30 @@
+// Add Express
+var express = require('express');
+var app = express();
+
+// Body Parser
+var bodyParser = require('body-parser');
+app.use( bodyParse.urlencoded({ extended: true }));
+app.use( bodyParser.json() );
+
+// Postgres Setup
+var pg = require( 'pg' );
+var connectionString = process.env.DATABASE_URL || 'postgres:localhost:5432/mr_muggles';
+
+// Open /public
+app.use( express.static( 'public ') );
+
+// Listen on port
+app.listen( port, function () {
+
+	console.log( "Server listening on port ", port );
+
+});
+
+// Serve Home Page
+app.get( '/', function( req, res ) {
+
+	console.log( 'Base URL hit' );
+	res.send( 'public/index.html' );
+
+});
